807933f9196115626cd986667091b14cbd065a92
wolf68065 <145383648+wolf68065@users.noreply.github.com>
2023-12-15 15:10:25 -0600

    Add files via upload

---
+++ src/main/resources/projects-schema.sql
@@ -0,0 +1,64 @@
+DROP TABLE IF EXISTS material;
+DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS project_category;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS project;
+
+CREATE TABLE project (
+project_id INT AUTO_INCREMENT NOT NULL,
+project_name VARCHAR(128) NOT NULL,
+estimated_hours DECIMAL (7, 2),
+actual_hours DECIMAL (7, 2),
+difficulty INT,
+notes TEXT,
+create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (project_id)
+);
+
+CREATE TABLE category (
+category_id INT AUTO_INCREMENT NOT NULL,
+category_name VARCHAR(128) NOT NULL,
+PRIMARY KEY (category_id)
+);
+
+CREATE TABLE project_category (
+project_id INT NOT NULL,
+category_id INT NOT NULL,
+FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE,
+FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE,
+UNIQUE KEY (project_id, category_id)
+);
+
+CREATE TABLE step (
+step_id INT AUTO_INCREMENT NOT NULL,
+project_id INT NOT NULL,
+step_text TEXT NOT NULL,
+step_order INT NOT NULL,
+PRIMARY KEY (step_id),
+FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
+CREATE TABLE material (
+material_id INT AUTO_INCREMENT NOT NULL,
+project_id INT NOT NULL,
+material_name VARCHAR(128) NOT NULL,
+num_required INT,
+cost DECIMAL(7, 2),
+PRIMARY KEY (material_id),
+FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
+INSERT INTO category(category_name)VALUES('Doors and Windows');
+SELECT*FROM project;
+INSERT INTO material(project_id, material_name, num_required)
+VALUES
+(1, '2-inch', 20);
+INSERT INTO step(project_id, step_text, step_order)
+VALUES
+(1, 'Screw door hangers on the top and bottom of each side of the door frame', 1);
+INSERT INTO project_category(project_id, category_id)
+VALUES
+(1,2);
+INSERT INTO project (project_name, estimated_hours, actual_hours, difficulty, notes) VALUES ('Hanging a door, 10 , 5, 5, Remember to measure');
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (1, 'Door Hanger', 3, 600);
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (1, 'Screws', 20, 5.00);
+INSERT INTO step (project_id, step_text, step_order) VALUES ( 1, 'Align hanger on open side', 1);
+INSERT INTO step (project_id, step_text, step_order) VALUES (1, 'Screw hangers into the frame', 2);
